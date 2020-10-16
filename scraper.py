6d9a26113bf6d5cac43ce2c79c62ea9cf74f094d
Akash Pattnaik <pattnaik@princeton.edu>
2020-10-16 10:27:18 -0400

    hyphen for Nathan Coulter-Nile

--- scraper.py
+++ scraper.py
@@ -24,7 +24,7 @@ def extract_batting_data(series_id, match_id):
                 if (cols[7] == '-'):
                     cols[7] = 100 # this is when a batsman has not faced a ball, set to 100 so points aren't deducted for SR
                 batsmen_df = batsmen_df.append(pd.Series(
-                [re.sub(r"\W+", ' ', cols[0].split("(c)")[0]).strip(), cols[1], 
+                [re.sub(r"[^\w-]+", ' ', cols[0].split("(c)")[0]).strip(), cols[1], 
                 int(cols[2]), int(cols[3]), int(cols[5]), int(cols[6]), float(cols[7]), i % 2], 
                 index=batsmen_df.columns ), ignore_index=True)
     return batsmen_df
@@ -62,7 +62,7 @@ def find_xis(names_dict, series_id, match_id):
         if len(rows) == 3:
             dnb_names = rows[1].find_all('span')
             dnb_names = [x.text.replace(u'\xa0', '').strip(' ,') for x in dnb_names]
-            dnb_names = [re.sub(r"\W+", ' ', i.split("(c)")[0]).strip() for i in dnb_names]
+            dnb_names = [re.sub(r"[^\w-]+", ' ', i.split("(c)")[0]).strip() for i in dnb_names]
             dnb_names = [i for i in dnb_names if i]
             xis.extend(dnb_names)
     
@@ -72,17 +72,17 @@ def find_xis(names_dict, series_id, match_id):
             cols=row.find_all('td')
             cols=[x.text.strip() for x in cols]
             if len(cols) >= 8:
-                xis.append(re.sub(r"\W+", ' ', cols[0].split("(c)")[0]).strip())
+                xis.append(re.sub(r"[^\w-]+", ' ', cols[0].split("(c)")[0]).strip())
             if len(cols) == 2:
                 if cols[1] == 'Batsman' or cols[1] == 'Allrounder' or cols[1] == 'Bowler' or cols[1] == 'Wicketkeeper':
-                    name = re.sub(r"\W+", ' ', cols[0].split("(c)")[0]).strip()
+                    name = re.sub(r"[^\w-]+", ' ', cols[0].split("(c)")[0]).strip()
                     xis.append(names_dict[name])
     if len(set(xis))  != 22:
         for i, table in enumerate(table_foot):
             rows = table.find_all('tr')
             dnb_names = rows[1].find_all('span')
             dnb_names = [x.text.replace(u'\xa0', '').strip(' ,') for x in dnb_names]
-            dnb_names = [re.sub(r"\W+", ' ', i.split("(c)")[0]).strip() for i in dnb_names]
+            dnb_names = [re.sub(r"[^\w-]+", ' ', i.split("(c)")[0]).strip() for i in dnb_names]
             dnb_names = [i for i in dnb_names if i]
             xis.extend(dnb_names)
 
